3abd1e5a46faa60f5442d36aca679745b3248fca
senenraketto <yantianhaoiwatasora@gmail.com>
2026-04-09 18:35:01 +0900

    空でだと登録できないように修正

--- db/migrate/20260319025242_devise_create_users.rb
+++ db/migrate/20260319025242_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name, null: false, default: ""
-      t.text :profile, null: false
+      t.text :profile, null: false 
       t.text :occupation, null: false 
       t.text :position, null: false
 
